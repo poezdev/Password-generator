c785b328939fb776f7f2bbd8ee22ba0ffa87c46c
Vanya <vanya.st.ov@gmail.com>
2021-02-04 17:05:54 +0300

    Css does not bundle. Began to fix problem

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isDev = proccess.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev; 
 
 module.exports = {
@@ -15,34 +15,72 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
 
-    //Добавить сервер
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
+        compress: true,
+        port: 8080,
+        hot: true,
+    },
 
     plugins: [
         new HTMLWebpackPlugin({
-            filename: 'index.html',
+            filename: `index.html`,
+            template: `${path.resolve(__dirname, '.')}/src/index.pug`
         }),
 
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
         new CleanWebpackPlugin(),
     ],
-    
+    devtool: isProd ? false : 'source-map',
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node-modules/,
                 use: ['babel-loader']
-
             },
             {
                 test: /\.(?:ico|gif|png|jpeg|jpg)$/i,
-                type: 'asset/resource',
-            }
-            //Добавить loader шрифты
-            //Добавить loader css scss
-            //Добавить loader pug
-            
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline'
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  { 
+                    loader: MiniCssExtractPlugin.loader,
+                  },
+                  {
+                    loader: 'style-loader',
+                  },
+                  {
+                    loader: 'css-loader',
+                  },
+                  {
+                    loader: 'resolve-url-loader',
+                  },
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      sourceMap: true,
+                    },
+                  },
+                ],
+              },
         ]
     }
-
 }
+
+// Разобраться со стилями - не появляется в dist 
+// Построить зависимости - 1) Импорт всех pug-компонентов в index.pug 2) Импорт всех sсss компонентов в js-
+// компоненты, а js-компоненты в index.js
